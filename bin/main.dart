fb6db2afc41b5ed431a9f09d9ce84d62d3088f6c
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-05-15 10:04:35 +0300

    7.1.1. What null means

--- bin/main.dart
+++ bin/main.dart
@@ -1,4 +1,19 @@
 // Dart Apprentice by Eng. Fady Fawzy
 // Chapter 7: Nullability
 
-void main() {}
+void main() {
+  // Null overview
+  whatNullMeans();
+}
+
+void whatNullMeans() {
+  int postalCode = 12345;
+
+  // Hey everybody, -1 means that the user
+  // doesn't have a postal code. Don't forget!
+  // int postalCode = -1;
+
+  //* In versions of Dart prior to 2.12 that line of code worked just fine.
+  // However, now it’s no longer allowed. You get the following error:
+  // int postalCode = null;
+}
